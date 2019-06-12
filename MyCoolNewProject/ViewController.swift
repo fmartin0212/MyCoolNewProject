0836d4b1335c98290680e8d3a1e88ee668bfcf32
Frank Martin <frankmartin@Franks-MacBook-Pro.local>
2019-06-12 12:26:55 -0600

    initial commit

---
+++ MyCoolNewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoolNewProject
+//
+//  Created by Frank Martin on 6/12/19.
+//  Copyright © 2019 Frank Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
